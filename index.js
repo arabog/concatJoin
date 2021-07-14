1743dd060164b5c13480c5a10cbf1c2de0b3ab0d
iCode_Rx <arababs2015@gmail.com>
2021-07-14 12:59:33 +0100

    soln

---
+++ index.js
@@ -0,0 +1,62 @@
+/*
+Coding in function bigToSmall. function accept 1 parameter arr(2D 
+number array).
+Your task is: First of all, refer to the example above, flat output arr 
+to a one-dimensional array.
+
+          var result="";
+          var cache=[];
+
+          for (var i = 0; i < 26; i++) 
+                    cache[i] = String.fromCharCode(97+i);
+
+          var result=cache.join("");
+          console.log(result) 
+
+
+And then sort array in descending order.
+Finally, use the separator ">" to connect the elements into a string.
+Don't complain about the situation like 1>1 is not reasonable, it is 
+just a separator.
+
+Some example:
+
+bigToSmall([[1,2],[3,4],[5,6]]) should return "6>5>4>3>2>1"
+bigToSmall([[1,3,5],[2,4,6]]) should return "6>5>4>3>2>1"
+bigToSmall([[1,1],[1],[1,1]]) should return "1>1>1>1>1"
+*/ 
+function bigToSmall(arr) {
+          // cr8 an empty arr
+          var cache=[];
+
+          // loop via d arr and mk dm into a single arr
+          for(let i = 0; i < arr.length; i++) {
+                    cache = cache.concat(arr[i])
+          }
+
+          // sort d single arr and chande to str using join with <
+          return cache = cache.sort((a, b) => b - a).join('>')
+}
+
+// anoda soln
+function bigToSmall(arr) {
+          return [].concat(...arr).sort((a, b) => b - a).join('>');
+}
+
+// anoda soln
+function bigToSmall(arr){
+          //coding here...
+          let res = [].concat(...arr);
+
+          res.sort(function(a, b){return a - b});
+
+          res.reverse();
+
+          res = res.join(">");
+          
+          return res;
+}
+
+bigToSmall([[1,2],[3,4],[5,6]])
+bigToSmall([[1,3,5],[2,4,6]]) 
+bigToSmall([[1,1],[1],[1,1]]) 
